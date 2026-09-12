8f3c39a5b8ca779bd142c77e8defd1b12df5e2cc
Arbin <arbinstyles146@gmail.com>
2026-09-12 18:27:30 +0545

    Add Link import to Home component and refactor router setup

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -5,24 +5,31 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom' 
 import Layout from './Layout.jsx'
 import Home from './component/Home/Home.jsx'
+import About from './component/About/About.jsx'
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path : "",
-        element : <Home />
-      }, {
-        path : "about",
-        element : <About />
-      }
-    ]
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path : "",
+//         element : <Home />
+//       }, {
+//         path : "about",
+//         element : <About />
+//       }
+//     ]
+//   },
+// ]);
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+  </Route>
+));
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
